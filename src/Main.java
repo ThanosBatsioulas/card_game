31035eed169de9c332f687101900889e1956c509
Thanos Bats <batsioularas@gmai.com>
2019-01-13 02:07:38 +0200

    completion of the Main class

--- src/Main.java
+++ src/Main.java
@@ -15,9 +15,9 @@ public class Main {
 
         Scanner ans = new Scanner(System.in);
         int i,j;
-        int num;
+        int num_players, num_bots, answer;
         Game game;
-        System.out.println("Θέλεις να παίξεις μόνος σου ή με άλλους;\nΓια solo πάτα 1 ,για multiplay πάτα 2");
+        System.out.println("Θέλεις να παίξεις μόνος σου ή με άλλους;\nΓια solo πάτα 1 ,για multiplayer πάτα 2\n");
         j = ans.nextInt();
         switch (j) {
             case 1: {
@@ -31,15 +31,15 @@ public class Main {
                     System.out.println();
                     switch (i) {
                         case 1: {
-                            game = new Game(4, 6, 2, 1);
+                            game = new Game(4, 6, 2, 1, 0);
                             break;
                         }
                         case 2: {
-                            game = new Game(6, 8, 2, 1);
+                            game = new Game(6, 8, 2, 1, 0);
                             break;
                         }
                         case 3: {
-                            game = new Game(6, 6, 3, 1);
+                            game = new Game(6, 6, 3, 1,0);
                             break;
                         }
                         case 0: {
@@ -51,9 +51,27 @@ public class Main {
             }
             case 2: {
                 do {
-                    System.out.println("Πόσοι παίχτες είστε; (μέχρι 2-4 άτομα)");
+                    do {
+                        System.out.println("Θέλεις να παίξεις ´μονομαχία´ ή multiplayer\nΓια μονομαχία πάτα 1\nΓια multiplayer πάτα 2\n");
+                        answer = ans.nextInt();
+                    } while (answer != 1 && answer != 2);
 
-                    num = ans.nextInt();
+                    if( answer == 1) {
+                        num_players = 2;
+                        num_bots = 0;
+                    }
+                    else {
+                        do {
+                            System.out.println("Πόσοι παίχτες είστε; (μέχρι 2-4 άτομα)");
+                            num_players = ans.nextInt();
+                        } while (num_players < 2 || num_players > 4);
+
+                        do {
+                            System.out.println("και πόσασ bot θέλεις να έχει το παιχνίδι;");
+                            num_bots = ans.nextInt();
+                        } while (num_bots >= num_players || num_bots <= 0);
+                    }
+
                     System.out.println("Πάτα 1 για το Βασικό\n");
                     System.out.println("Πάτα 2 για το Διπλάσιο\n");
                     System.out.println("Πάτα 3 για το Τρίο\n");
@@ -63,15 +81,15 @@ public class Main {
                     System.out.println();
                     switch (i) {
                         case 1: {
-                            game = new Game(4, 6, 2, num);
+                            game = new Game(4, 6, 2, num_players, num_bots);
                             break;
                         }
                         case 2: {
-                            game = new Game(6, 8, 2, num);
+                            game = new Game(6, 8, 2, num_players, num_bots);
                             break;
                         }
                         case 3: {
-                            game = new Game(6, 6, 3, num);
+                            game = new Game(6, 6, 3, num_players, num_bots);
                             break;
                         }
                         case 0: {
@@ -80,8 +98,6 @@ public class Main {
                         }
                     }
                 } while (i != 0);
-
-
             }
         }
     }
